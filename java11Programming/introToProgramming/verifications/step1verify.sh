9eec2cc13bf17c08b59742239e84d7b3136b56be
djafaka <kdjaafar@jasmineconseil.com>
2022-01-07 15:51:36 -0500

    add Java 11

--- java11Programming/introToProgramming/verifications/step1verify.sh
+++
@@ -1,2 +0,0 @@
-# Make sure the java file has been compiled. Not really checking content here.
-[ -f ./HelloWorld.class ] && echo "done"
--- java11Programming/introToProgrammingPart2/assets/NumberFun.java
+++
@@ -1,5 +0,0 @@
-public class NumberFun {
-  public static void main(String[] args) {
-    // Put your code here.
-  }
-}
